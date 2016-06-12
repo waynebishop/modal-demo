aff122a1fee7efeea7288ea7da8ae4d55ff56bc9
Wayne Bishop <bishopwb66@gmail.com>
2016-06-12 14:03:33 +1200

    Initial commit

---
+++ js/modal-window.js
@@ -0,0 +1,34 @@
+var model = (function() {								// Declare modal object
+	var $window = $(window);
+	var $modal = $('<div class="modal"/>');				// Create markup for modal
+	var $content = $('<div class="modal-content"/>');
+	var $close= $('<button role="button" class="modal-close">close</button>');
+
+	$modal.append($content, $close);					// Add close button to modal
+
+	$close.on('click', function(e) {					// If user clicks on close
+		e.preventDefault();								// Prevent link behaviour
+		modal.close();									// Close the modal window
+	});
+
+	return {											// Add code to modal
+		center: function() {							// Define center() method
+			// Calculate distance form top and left of window to center the modal
+			var top = Math.max($window.height() - $modal.outerHeight(), 0) / 2;
+			var left = Math.max($window.width() - $modal.outerWidth(), 0) / 2;
+			$modal.css({								// Set CSS for the modal
+				top: top + $window.scrollTop(),			// Center vertically
+				left: left + $window.scrollLeft()		// Center horizontally
+			})
+		},
+		open: function(settings)	{					// Define open() method
+			$content.empty().append(settings.content)	// Set new content of modal
+
+			$modal.css({})
+
+		}
+
+	}
+
+
+})
